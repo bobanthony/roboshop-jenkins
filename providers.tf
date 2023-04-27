c1cde628929307bed8fa09d1a8fc113cdc4e8ce5
Dinesh <satyadineshk328@gmail.com>
2023-04-27 17:01:53 +0530

    j

--- providers.tf
+++ providers.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    jenkins = {
+      source = "registry.terraform.io/taiidani/jenkins"
+    }
+  }
+}
+
+
+
+
+
 provider "jenkins" {
   server_url = "https://44.192.89.199:8080"
   username   = data.aws_ssm_parameter.jenkins_user
