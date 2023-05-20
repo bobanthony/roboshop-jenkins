edf9ed9364ec62f53d73d0048156f26447097f54
Dinesh <satyadineshk328@gmail.com>
2023-05-20 15:12:39 +0530

    j

--- vars.tf
+++ vars.tf
@@ -4,21 +4,21 @@ variable "folders" {
 
 variable "m-jobs" {
   default = [
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/frontend" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/cart" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/catalogue" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/user" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/shipping" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/payment" },
-    { name = "aws-ssm-param-store", folder = "CI-Pipeline", repo_url = "https://github.com/raghudevopsb71/aws-ssm-param-store" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/bobanthony/frontend" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/bobanthony/cart" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/bobanthony/catalogue" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/bobanthony/user" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/bobanthony/shipping" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/bobanthony/payment" },
+    { name = "aws-ssm-param-store", folder = "CI-Pipeline", repo_url = "https://github.com/bobanthony/aws-ssm-param-store" }
   ]
 }
 
 variable "s-jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-infra", filename = "Jenkinsfile" },
-    { name = "App-Deployment", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-ansible", filename = "Jenkinsfile-deployment" },
-    { name = "Helm-Deployment", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-helm-chart", filename = "Jenkinsfile" }
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/bobanthony/roboshop-infra", filename = "Jenkinsfile" },
+    { name = "App-Deployment", folder = "Infra", repo_url = "https://github.com/bobanthony/roboshop-ansible", filename = "Jenkinsfile-deployment" },
+    { name = "Helm-Deployment", folder = "Infra", repo_url = "https://github.com/bobanthony/roboshop-helm-chart", filename = "Jenkinsfile" }
   ]
 }
 
